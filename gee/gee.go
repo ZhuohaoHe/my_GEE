66c5da19fc2f1b9641026aa48bded29c111a70ab
ZhuohaoHe <zhuohao.he@outlook.com>
2023-03-02 19:52:09 +0800

    day1-HTTP

---
+++ gee/gee.go
@@ -0,0 +1,55 @@
+package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	// routing mapping table:
+	// 	k: request method and static routing address -> Handler
+	// 		such as `GET-/`, `GET-/hello`, `POST-/hello`
+	//  value: handler method that defined by user
+	router map[string]HandlerFunc
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// Parse the path of the request
+// search the route mapping table
+// and if found, execute the registered processing method
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
